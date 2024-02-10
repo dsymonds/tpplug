354fa30b369c8c1b768710ab64a671b1e59a18a3
David Symonds <dsymonds@gmail.com>
2024-02-10 20:31:23 +1100

    Add history tracking to the Prometheus exporter.
    This should help cope with plugs that stop being discoverable, but
    remain directly contactable.

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -17,6 +19,7 @@ import (
 var (
 	port     = flag.Int("port", 0, "port to run on")
 	scanTime = flag.Duration("scan_time", 2*time.Second, "how long to wait for discovery")
+	history  = flag.Duration("history", 10*time.Minute, "how long to keep trying to contact a plug that stopped responding")
 )
 
 func main() {
@@ -30,6 +33,8 @@ func main() {
 
 // dataCollector implements prometheus.Collector.
 type dataCollector struct {
+	mu   sync.Mutex
+	prev map[string]macInfo
 }
 
 var (
@@ -62,26 +67,68 @@ func (dc *dataCollector) Collect(ch chan<- prometheus.Metric) {
 		okDesc, prometheus.GaugeValue, ok)
 }
 
+// macInfo represents a previously seen plug.
+type macInfo struct {
+	addr *net.UDPAddr
+	t    time.Time
+}
+
 func (dc *dataCollector) collect(ch chan<- prometheus.Metric) error {
 	ctx, cancel := context.WithTimeout(context.Background(), *scanTime)
 	defer cancel()
+
+	sendPower := func(state tpplug.State, addr *net.UDPAddr) {
+		info := state.System.Info
+		rt := state.EnergyMeter.Realtime
+		//log.Printf("(%s, %s) %q: %.1f W", info.MAC, addr, info.Alias, float64(rt.Power)/1000)
+
+		ch <- prometheus.MustNewConstMetric(
+			powerDesc, prometheus.GaugeValue,
+			float64(rt.Power),
+			info.MAC, addr.IP.String(), info.Alias)
+	}
 
 	drs, err := tpplug.Discover(ctx)
 	if err != nil {
 		return err
 	}
+	macs := make(map[string]macInfo)
+	now := time.Now()
 	for _, dr := range drs {
-		state := dr.State
-		info := state.System.Info
-		rt := state.EnergyMeter.Realtime
-		//log.Printf("(%s, %s) %q: %.1f W", info.MAC, dr.Addr, info.Alias, float64(rt.Power)/1000)
-
-		ip := dr.Addr.IP.String()
-
-		ch <- prometheus.MustNewConstMetric(
-			powerDesc, prometheus.GaugeValue,
-			float64(rt.Power),
-			info.MAC, ip, info.Alias)
+		macs[dr.State.System.Info.MAC] = macInfo{addr: dr.Addr, t: now}
+		sendPower(dr.State, dr.Addr)
 	}
+
+	// Query MACs that we saw last time but didn't see this time.
+	dc.mu.Lock()
+	prev := dc.prev
+	dc.mu.Unlock()
+	for mac, info := range prev {
+		if _, ok := macs[mac]; ok {
+			continue
+		}
+		if now.Sub(info.t) > *history {
+			continue
+		}
+
+		// TODO: Controllable?
+		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+		state, err := tpplug.Query(ctx, info.addr)
+		cancel()
+		if err == nil {
+			macs[mac] = macInfo{addr: info.addr, t: now}
+			sendPower(state, info.addr)
+		} else {
+			// Keep remembering it for now; it'll age out eventually if it never responds.
+			macs[mac] = info
+		}
+	}
+
+	// Remember the set of responding plugs and the ones that aren't
+	// responding but did within the history interval.
+	dc.mu.Lock()
+	dc.prev = macs
+	dc.mu.Unlock()
+
 	return nil
 }
